3ddc93801e9f9caeeba83a9de6d3bcaff7f2991b
Sarah Maas <maass@oregonstate.edu>
2018-02-19 03:49:02 +0000

    adventurer random test complete with coverage results

--- projects/maass/dominion/randomtestadventurer.c
+++ projects/maass/dominion/randomtestadventurer.c
@@ -6,12 +6,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NUM_TESTS 500
+#define NUM_TESTS 1000
 
 // testing adventurer card
 
 int asserttrue(int statement);
-
+int countDeckTreasure(struct gameState *state);
 
 int main(int argc, char* argv[]) {
 	time_t t;
@@ -39,21 +39,19 @@ int main(int argc, char* argv[]) {
 
         // init game
         initializeGame(players, k, seed, &state);
-    
+        
         // What is the expected behavior??
         //  +2 treasure if they exist
+        
     
         int handCountBefore = numHandCards(&state);
-    
-        // printf("\tplay adventurer successfully executes:\n");
-        // fflush(stdout); 
+        int deckTreasureBefore = countDeckTreasure(&state);
         int played =  (cardEffect(adventurer, choice1, choice2, choice3, &state, handPos, &bonus) == 0);
         execSuccess += asserttrue(played);
         int handCountAfter = numHandCards(&state);
-        
-        // printf("\tcorrect number of cards in hand:\n");
-        // fflush(stdout);     
-        cardSuccess += asserttrue((handCountAfter - handCountBefore) == 2);
+        //int deckTreasureAfter = countDeckTreasure(&state);
+        int cardsToDraw = (deckTreasureBefore > 2) ? 2: deckTreasureBefore;
+        cardSuccess += asserttrue((handCountAfter - handCountBefore) == cardsToDraw);
     }
 	// tallied results
     printf("\tplay adventurer successfully executed: %d of %d times.\n", execSuccess, NUM_TESTS);
@@ -74,4 +72,23 @@ int asserttrue(int statement) {
     } else {
         return FALSE;
     }
+}
+
+
+// counts the treasure that can be drawn
+int countDeckTreasure(struct gameState *state) {
+    int count = 0;
+
+    int i;
+    for (i = 0; i < state->deckCount[state->whoseTurn]; i++) {
+        // test if treasure
+        switch(state->deck[state->whoseTurn][i]) {
+            case copper:
+            case silver:
+            case gold:
+                count++;
+        }
+    }
+    
+    return count;
 }
