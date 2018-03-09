cb8a9d1e0852a1215426b2450a84869c31b03577
Colin Powell <colingrahampowell@gmail.com>
2018-03-09 17:00:42 -0500

    adding more detailed manual tests, changing UrlValidator constructor to explicitly cover http, https, and ftp

--- projects/powelcol/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/powelcol/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -46,33 +46,54 @@ public class UrlValidatorTest {
    		System.out.println("********Manual Tests********");
 		
 		System.out.println("Testing URLs...");
-   		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+		String[] schemes = { "http", "https", "ftp" };
+   		UrlValidator urlVal = new UrlValidator(schemes);
    	
    		// basic manual tests: 
+   		// DEBUGGING NOTE: check UrlValidator line 282; schemes are converted to upper case 
+   		//				   instead of lower case, as required by isValid...
+   		//				   if ALLOW_ALL_SCHEMES flag is not set, this will cause all tests below to fail.
    		
-   		collector.checkThat("expect basic URL to be valid", 
-   				  urlVal.isValid("http://www.google.com"), CoreMatchers.equalTo(true));
+   		collector.checkThat("expect basic http-based URL to be valid", 
+   				  	urlVal.isValid("http://www.google.com"), CoreMatchers.equalTo(true));
+   		
+   		collector.checkThat("expect https to be recognized", 
+   					urlVal.isValid("https://www.google.com"), CoreMatchers.equalTo(true));
    		
    		collector.checkThat("expect port numbers to be valid", 
-   				  urlVal.isValid("http://www.google.com:65530"), CoreMatchers.equalTo(true));
+   				  	urlVal.isValid("http://www.google.com:65530"), CoreMatchers.equalTo(true));
    		
    		collector.checkThat("expect query string to register as valid", 
-   				  urlVal.isValid("http://www.google.com?foo=bar&baz=bam"), CoreMatchers.equalTo(true));
+   				  	urlVal.isValid("http://www.google.com?foo=bar&baz=bam"), CoreMatchers.equalTo(true));
    		
    		//RFC 1123: hostnames must be alphanumeric:
    		
    		collector.checkThat("expect numeric chars in hostname to register as valid", 
-   				  urlVal.isValid("http://www.g00gle.com"), CoreMatchers.equalTo(true));
+   				  	urlVal.isValid("http://www.g00gle.com"), CoreMatchers.equalTo(true));
    		
    		collector.checkThat("expect invalid TLD chars to register as invalid", 
-   				  urlVal.isValid("http://www.google.c!m"), CoreMatchers.equalTo(false));
+   				  	urlVal.isValid("http://www.google.c!m"), CoreMatchers.equalTo(false));
    		
    		collector.checkThat("expect invalid hostname chars to register as invalid", 
-   				  urlVal.isValid("http://www.g!.com"), CoreMatchers.equalTo(false));
-   	 		
-   		//These actually cause errors:
-//   	collector.checkThat(urlVal.isValid("ftp://ftp.filestorage.com:5440"), CoreMatchers.equalTo(true));
-//		collector.checkThat(urlVal.isValid("ftp://ftp.foo.com/file.txt"), CoreMatchers.equalTo(true));
+   					urlVal.isValid("http://www.g!.com"), CoreMatchers.equalTo(false));
+   		 		
+   		// non-http protocols seem to be an issue, esp. with ALLOW_ALL_SCHEMES flag set:
+   		// wrap in try/catch blocks to catch errors without terminating program
+   		try {
+   			collector.checkThat("expect ftp scheme to register as valid", 
+   					urlVal.isValid("ftp://ftp.filestorage.com:5440"), CoreMatchers.equalTo(true));
+   		} 
+   		catch(Throwable err) {
+   			collector.addError(err);
+   		}
+  
+ 	 	try {
+ 	 		collector.checkThat("expect ftp scheme to register as valid", 
+ 	 				urlVal.isValid("ftp://foo.bar.com/"), CoreMatchers.equalTo(true));
+ 	 	}
+ 	 	catch(Throwable err) {
+ 	 		collector.addError(err);
+ 	 	}		
 	   
    		System.out.println("Manual Tests Complete");
    }
